c66dddd4c8eeaab55d96081d3e46e4a5065fa84f
SnappyKernelX <varadpro.dev@gmail.com>
2026-08-12 13:06:27 +0530

    Create filter.c

---
+++ filter.c
@@ -0,0 +1,130 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "helpers.h"
+
+int main(int argc, char *argv[])
+{
+    char *filters = "begr";
+
+    char filter = getopt(argc, argv, filters);
+    if (filter == '?')
+    {
+        printf("Invalid filter.\n");
+        return 1;
+    }
+
+    if (getopt(argc, argv, filters) != -1)
+    {
+        printf("Only one filter allowed.\n");
+        return 2;
+    }
+
+    if (argc != optind + 2)
+    {
+        printf("Usage: ./filter [flag] infile outfile\n");
+        return 3;
+    }
+
+    char *infile = argv[optind];
+    char *outfile = argv[optind + 1];
+
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 4;
+    }
+
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        printf("Could not create %s.\n", outfile);
+        return 5;
+    }
+
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        printf("Unsupported file format.\n");
+        return 6;
+    }
+
+    // Get image's dimensions
+    int height = abs(bi.biHeight);
+    int width = bi.biWidth;
+
+    // Allocate memory for image
+    RGBTRIPLE(*image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    if (image == NULL)
+    {
+        printf("Not enough memory to store image.\n");
+        fclose(outptr);
+        fclose(inptr);
+        return 7;
+    }
+
+    // Determine padding for scanlines
+    int padding = (4 - (width * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // Iterate over infile's scanlines
+    for (int i = 0; i < height; i++)
+    {
+        // Read row into pixel array
+        fread(image[i], sizeof(RGBTRIPLE), width, inptr);
+        fseek(inptr, padding, SEEK_CUR);
+    }
+
+    // Filters:
+    switch (filter)
+    {
+        case 'b':
+            blur(height, width, image);
+            break;
+      
+        case 'e':
+            edges(height, width, image);
+            break;
+
+        case 'g':
+            grayscale(height, width, image);
+            break;
+
+        case 'r':
+            reflect(height, width, image);
+            break;
+    }
+
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    for (int i = 0; i < height; i++)
+    {
+        fwrite(image[i], sizeof(RGBTRIPLE), width, outptr);
+
+        for (int k = 0; k < padding; k++)
+        {
+            fputc(0x00, outptr);
+        }
+    }
+
+    // Free memory for image
+    free(image);
+
+    // Close files
+    fclose(inptr);
+    fclose(outptr);
+    return 0;
+}
